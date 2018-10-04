47dcec22513eb306e84ee244c6727278a02cd1f4
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2018-10-04 13:57:35 +0200

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,16 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @RestController
 public class DemoApplication {
 
 	@GetMapping("/")
 	public String getHome(){
-		return "Hello World";
+		return "Bonjour Adrien";
 	}
 	
 	public static void main(String[] args) {
